50ab234ff239f5389ee419849098c99b54490367
Eric Munyi <irerieric@gmail.com>
2024-01-16 21:25:58 +0300

    Listing

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    image_url = models.URLField()
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='listings')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
