ed0249f6714bfc44b06514cdde0e79eab264b92e
Eric Munyi <irerieric@gmail.com>
2024-02-09 21:04:31 +0300

    update urls

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
-    path('create_listing/', create_listing, name='create_listing')
+    path('create_listing/', views.create_listing, name='create_listing'),
+    path("active_listing/", views.active_listings, name="active_listing")
 ]
