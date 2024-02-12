0641ae0edea2ce2bc0c9c87711efaef01f252c36
Eric Munyi <irerieric@gmail.com>
2024-02-12 13:24:45 +0300

    fixed a bug

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Listing
+from .models import Listing, Bid, Comment
 
 
 class ListingForm(forms.ModelForm):
@@ -7,3 +7,17 @@ class ListingForm(forms.ModelForm):
         model = Listing
         fields = ['title', 'description',
                   'starting_bid', 'image_url', 'category']
+
+
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid  # Add this line
+        fields = ['amount']
+        labels = {'bid_amount': 'Bid Amount'}
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment  # Add this line
+        fields = ['content']
+        labels = {'content': 'Comment'}
